50b41ee53aa364c7898c8c34f3e6f159583a6314
Joao <joao-victor1982@hotmail.com>
2024-11-21 20:52:21 -0300

    many things

---
+++ blog/views.py
@@ -0,0 +1,36 @@
+from django.core.paginator import Paginator
+from django.shortcuts import render
+from blog.models import Post
+# Create your views here.
+
+PER_PAGE = 9
+
+
+def index(request):
+    posts = Post.objects.get_published()  # type: ignore
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+
+def page(request):
+    return render(
+        request,
+        'blog/pages/page.html'
+    )
+
+
+def post(request):
+    return render(
+        request,
+        'blog/pages/post.html'
+    )
